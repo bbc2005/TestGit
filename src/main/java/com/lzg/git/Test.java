3e04c37953b2ef73bf2fb322e73ec5d7bde9eb6a
bbc2005 <bbc20051014@163.com>
2019-12-03 22:21:40 +0800

    首次提交

---
+++ src/main/java/com/lzg/git/Test.java
@@ -0,0 +1,15 @@
+package com.lzg.git;
+/**
+ * 
+ * @author lzg
+ * @date 2019-12-03 22:17:57
+ *
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
